7fc3265930ad8e87eee018115a00f465d8f9f377
Shen Wei <weix.shen@intel.com>
2016-01-08 18:39:06 +0800

    Add generator exercise from office

---
+++ about_python/x_generator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys
+import pdb
+
+pdb.set_trace()
+
+
+def fibo_gen(n):
+    a = b = 1
+    for i in range(n):
+        yield a
+        a, b = b, a + b
+
+
+def fibo_iter(n):
+    a = b = 1
+    l = []
+    for i in range(n):
+        l.append(a)
+        a, b = b, a + b
+    return l
+
+
+method_used = sys.argv[1]
+method_used = 'fibo_iter(3)' if method_used == 'iter' else 'fibo_gen(3)'
+for item in eval(method_used):
+    print item
